0d337a4aeed5cfb29f10a68d8e1892c4c4929bd0
kailafitz <mikhailafitzpatrick@yahoo.com>
2023-07-11 23:16:56 +0100

    Update of packages

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,9 +7,6 @@ import Navigation from "./Components/Navigation";
 import "../styles/styles.css";
 import { Footer } from "./Components/Footer";
 import { QueryClient, QueryClientProvider } from "react-query";
-import "@fortawesome/fontawesome-svg-core/styles.css";
-import { config } from "@fortawesome/fontawesome-svg-core";
-config.autoAddCss = false;
 
 const inter = Inter({ subsets: ["latin"] });
 
