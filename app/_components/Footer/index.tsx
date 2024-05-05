5104705ebf57f33f916becd3f6bf10609593b6fd
kailafitz <mikhailafitzpatrick@yahoo.com>
2024-05-06 00:14:05 +0800

    Cookies link hidden temporarily

--- app/_components/Footer/index.tsx
+++ app/_components/Footer/index.tsx
@@ -18,17 +18,17 @@ export const Footer = () => {
         >
           <Typography variant="body1">
             <Link color="common.white" href="privacy-policy">
-              Privacy
+              Privacy Policy
             </Link>
           </Typography>
-          <Typography variant="body1" color="common.white" mx={1}>
+          {/* <Typography variant="body1" color="common.white" mx={1}>
             |
           </Typography>
           <Typography variant="body1">
             <Link color="common.white" href="cookies-policy">
               Cookies
             </Link>
-          </Typography>
+          </Typography> */}
         </Box>
 
         <Typography
