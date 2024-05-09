b68e0c6ba0b6de40cd0392e1950915c7b643b645
kailafitz <mikhailafitzpatrick@yahoo.com>
2024-05-09 20:45:50 +0800

    Dribble section sorted

--- app/_components/Dribbble/Shots/index.tsx
+++ app/_components/Dribbble/Shots/index.tsx
@@ -1,31 +1,21 @@
-"use client";
-import React, { useEffect } from "react";
+import React from "react";
 import Image from "next/image";
 
-const Shots = () => {
+const GetData = async ({ token }: { token: string }) => {
+  const res = await fetch(
+    `https://api.dribbble.com/v2/user/shots?access_token=${token}`
+  );
+  return res.json();
+};
+
+const Shots = async () => {
   let accessToken =
     "80cdba96a6100c8df4e2526e289935e5e5f13dffc35392e443c91236f17ceeb6";
-
-  useEffect(() => {
-    const Data = async () => {
-      const res = await fetch(
-        `https://api.dribbble.com/v2/user/shots?access_token=80cdba96a6100c8df4e2526e289935e5e5f13dffc35392e443c91236f17ceeb6`
-      )
-        .then((res) => {
-          console.log(res.json());
-          // return res.json();
-        })
-        .catch((error) => console.log(error));
-
-      // return res.data.slice(0, 2);
-    };
-
-    Data();
-  }, []);
+  const data = await GetData({ token: accessToken });
 
   return (
     <>
-      {/* {data.map((shot: { images: { normal: any } }, i: number) => {
+      {data.slice(0, 2).map((shot: { images: { normal: any } }, i: number) => {
         return (
           <Image
             key={i}
@@ -36,8 +26,7 @@ const Shots = () => {
             className="shadow h-auto"
           />
         );
-      })} */}
-      <h1>Hi</h1>
+      })}
     </>
   );
 };
--- app/page.tsx
+++ app/page.tsx
@@ -15,7 +15,7 @@ export default function Home() {
     <main>
       <Intro />
       <Projects />
-      {/* <Dribbble /> */}
+      <Dribbble />
       <Instagram />
       <Contact />
     </main>
